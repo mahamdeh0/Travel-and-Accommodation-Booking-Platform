19a7a59bfdf3fdb7dc0432669645b6bd336b4359
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-04 11:27:03 +0200

    Add Image class with EntityId, Type, and Path properties, introduce ImageType enum

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Image .cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Entities
+{
+    public class Image : EntityBase
+    {
+        public Guid EntityId { get; set; }
+        public ImageType Type { get; set; }
+        public string Path { get; set; }
+    }
+}
