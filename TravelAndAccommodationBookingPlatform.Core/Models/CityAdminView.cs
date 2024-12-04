9192660035811362a4e6572ef849fff44c86db49
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-04 15:46:09 +0200

    Add CityAdminView model

---
+++ TravelAndAccommodationBookingPlatform.Core/Models/CityAdminView.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Models
+{
+    public class CityAdminView
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int TotalHotels { get; set; }
+        public string Country { get; set; }
+        public string PostOffice { get; set; }
+        public string Region { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
