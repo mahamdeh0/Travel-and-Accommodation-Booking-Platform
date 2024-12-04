e285b710802b06f7fd41a989781443aa9b0d041b
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-04 11:43:25 +0200

    Add Hotel, Review classes and HotelStatus enum, establish relationships with Owner, City, and other entities

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities
+{
+    public class Review : EntityBase, IAuditableEntity
+    {
+        public Guid GuestId { get; set; }
+        public User Guest { get; set; }
+        public Guid HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Core/Enums/HotelStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Enums
+{
+    public enum HotelStatus
+    {
+        Open = 0,
+        Closed = 1,
+        UnderMaintenance = 2
+    }
+}
