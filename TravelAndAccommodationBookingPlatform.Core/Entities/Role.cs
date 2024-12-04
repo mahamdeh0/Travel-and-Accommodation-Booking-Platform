bcb353beb578d13a2654ed3f09d6af0c0e780a35
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-04 11:14:51 +0200

    Add User and Role entities with basic properties and relationships

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities
+{
+    public class Role : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
