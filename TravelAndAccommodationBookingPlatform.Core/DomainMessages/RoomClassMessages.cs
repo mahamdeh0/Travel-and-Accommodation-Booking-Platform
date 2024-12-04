74f9c7f7c8eb7b07d35039049e415f03ec4b3bd9
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-04 13:48:15 +0200

    Add RoomClassMessages class with constant strings for room class-related messages

---
+++ TravelAndAccommodationBookingPlatform.Core/DomainMessages/RoomClassMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.DomainMessages
+{
+    public static class RoomClassMessages
+    {
+        public const string RoomClassNotFound = "The room class with the provided ID could not be found.";
+        public const string RoomClassNameExistsInHotel = "A room class with the same name already exists in this hotel. Please choose a different name.";
+        public const string ExistingRoomsForRoomClass = "There are existing rooms assigned to the specified room class.";
+        public const string RoomNotFoundInRoomClass = "The specified room could not be found in the provided room class.";
+        public const string DuplicatedRoomNumber = "A room with the same number already exists in the specified room class.";
+    }
+}
