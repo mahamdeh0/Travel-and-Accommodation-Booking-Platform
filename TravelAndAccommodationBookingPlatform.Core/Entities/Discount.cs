12368b87ca97c275cfa03249ed248dd175c82137
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-04 11:58:40 +0200

    Add Discount, Room, RoomClass entities and RoomType enum

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Discount.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities
+{
+    public class Discount : EntityBase
+    {
+        public Guid RoomClassId { get; set; }
+        public RoomClass RoomClass { get; set; }
+        public decimal Percentage { get; set; }
+        public string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
