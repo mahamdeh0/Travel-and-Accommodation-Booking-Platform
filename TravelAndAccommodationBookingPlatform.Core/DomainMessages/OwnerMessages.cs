c51096815350caddce368385a9e4bc956de95a21
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-04 13:35:55 +0200

    Add OwnerMessages class with constant strings for owner-related messages

---
+++ TravelAndAccommodationBookingPlatform.Core/DomainMessages/OwnerMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.DomainMessages
+{
+    public static class OwnerMessages
+    {
+        public const string OwnerNotFound = "Owner with the provided ID is not found.";
+        public const string EmailAlreadyExists = "An owner with the provided email address already exists.";
+        public const string InvalidOwnerDetails = "The provided owner details are invalid. Please check the input and try again.";
+        public const string DeletionNotAllowed = "The owner cannot be deleted because there are existing hotels or bookings associated with them.";
+    }
+}
