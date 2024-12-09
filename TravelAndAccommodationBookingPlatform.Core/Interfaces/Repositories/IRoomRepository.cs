cc49651d1132bc17c33d7792548827f79b246ac3
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-09 10:28:01 +0200

    Add IRoomRepository interface for room-related persistence operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+{
+    public interface IRoomRepository
+    {
+        Task<Room> AddRoomAsync(Room room);
+        Task UpdateRoomAsync(Room room);
+        Task RemoveRoomAsync(Guid id);
+        Task<bool> ExistsByPredicateAsync(Expression<Func<Room, bool>> predicate);
+        Task<PaginatedResult<Room>> GetRoomsAsync(PaginatedQuery<Room> query);
+        Task<Room?> GetRoomByIdAsync(Guid roomClassId, Guid id);
+        Task<Room?> GetRoomWithRoomClassByIdAsync(Guid roomId);
+        Task<PaginatedResult<HotelManagementDto>> GetRoomsForManagementAsync(PaginatedQuery<Room> query);
+    }
+}
