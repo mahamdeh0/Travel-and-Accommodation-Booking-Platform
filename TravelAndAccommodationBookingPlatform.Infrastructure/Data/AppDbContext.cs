bd99d3b07440d15fa5094366ccd992a860f74cee
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-09 13:39:11 +0200

    Added AppDbContext class to configure DbSets and apply entity configurations

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Configurations;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<InvoiceDetail> InvoiceDetail { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<RoomClass> RoomClasses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookingConfiguration).Assembly);
+
+        }
+    }
+}
