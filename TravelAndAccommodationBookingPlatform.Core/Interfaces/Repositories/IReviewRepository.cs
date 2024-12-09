6ab303bbc593a8cd4ab1cb690680124c27c023a4
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-09 10:39:49 +0200

    Add IReviewRepository interface for review-related persistence operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IReviewRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+{
+    public interface IReviewRepository
+    {
+        Task<Review> AddReviewAsync(Review review);
+        Task<PaginatedResult<Review>> GetReviewsAsync(PaginatedQuery<Review> query);
+        Task<Review?> GetReviewByIdAsync(Guid hotelId, Guid reviewId);
+        Task<Review?> GetReviewByIdAsync(Guid reviewId, Guid hotelId, Guid guestId);
+        Task<int> GetHotelRatingAsync(Guid hotelId);
+        Task<int> GetHotelReviewCountAsync(Guid hotelId);
+        Task RemoveReviewAsync(Guid reviewId);
+        Task UpdateReviewAsync(Review review);
+        Task<bool> ExistsAsync(Expression<Func<Review, bool>> predicate);
+    }
+}
