c16d92248b7db86dad9df25b791977a92d3647f5
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-09 10:00:02 +0200

    Add IOwnerRepository interface for owner-related persistence operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IOwnerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+{
+    /// <summary>
+    /// Interface for managing Owner-related persistence operations.
+    /// </summary>
+    public interface IOwnerRepository
+    {
+        Task<Owner> AddOwnerAsync(Owner owner);
+        Task<PaginatedResult<Owner>> GetOwnersAsync(PaginatedQuery<Owner> query);
+        Task UpdateOwnerAsync(Owner owner);
+        Task<Owner?> GetOwnerByIdAsync(Guid id);
+        Task<bool> OwnerExistsAsync(Expression<Func<Owner, bool>> predicate);
+    }
+}
