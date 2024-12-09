b63a529e19dbdeba27592a91fa2943ab3256d857
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-09 13:50:34 +0200

    Added JwtSettings class for JWT configuration

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Auth/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Infrastructure.Auth
+{
+    public class JwtSettings
+    {
+        public required string Key { get; set; }
+        public required string Issuer { get; set; }
+        public required string Audience { get; set; }
+        public required double LifetimeMinutes { get; set; }
+    }
+}
