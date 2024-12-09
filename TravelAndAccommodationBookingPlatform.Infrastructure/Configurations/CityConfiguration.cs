6109602408ca4b6c3b9b9b6d682d5eae655f8535
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-09 13:15:36 +0200

    Added CityConfiguration class for entity configuration

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Configurations/CityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Configurations
+{
+    public class CityConfiguration : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Ignore(c => c.SmallPreview);
+
+            builder.HasMany(h => h.Hotels).WithOne(c => c.City).IsRequired()
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
