69812d2bcfa867fc81313b351a52b2a22d0a5640
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-10 14:06:27 +0200

    Refactor DTO classes for better naming consistency

--- TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/ICityRepository.cs
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/ICityRepository.cs
@@ -7,14 +7,14 @@ namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
     /// <summary>
     /// Interface for managing city-related persistence operations.
     /// </summary>
-    internal interface ICityRepository
+    public interface ICityRepository
     {
         Task<City?> GetByIdAsync(Guid id);
         Task<City> AddAsync(City city);
         Task RemoveAsync(Guid id);
         Task UpdateAsync(City city);
         Task<bool> ExistsAsync(Expression<Func<City, bool>> predicate);
-        Task<PaginatedResult<CityAdminView>> GetCitiesForAdminAsync(PaginatedQuery<City> PaginatedQuery);
+        Task<PaginatedResult<CityManagementDto>> GetCitiesForAdminAsync(PaginatedQuery<City> PaginatedQuery);
         Task<IEnumerable<City>> GetTopMostVisitedAsync(int count);
     }
 }
