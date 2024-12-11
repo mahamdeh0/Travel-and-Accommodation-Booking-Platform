79f317b82bb6360877c78acdd3a672dc1df57665
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-11 17:26:31 +0200

    Add DeleteCityCommand and DeleteCityCommandHandler for city deletion functionality

---
+++ TravelAndAccommodationBookingPlatform.Application/Commands/CityCommands/DeleteCityCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Commands.CityCommands
+{
+    public class DeleteCityCommand : IRequest
+    {
+        public Guid CityId { get; init; }
+    }
+}
