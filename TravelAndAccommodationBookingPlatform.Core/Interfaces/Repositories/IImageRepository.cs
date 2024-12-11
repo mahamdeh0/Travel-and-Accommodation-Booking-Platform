2ec2f088ec4e93259c2aeb019aa430289bc98080
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-11 10:35:04 +0200

    Add IImageRepository interface with methods for uploading and removing images

--- TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IImageRepository.cs
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IImageRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+﻿using Microsoft.AspNetCore.Http;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
 {
     public interface IImageRepository
     {
+        Task<Image> UploadImageAsync(IFormFile image, Guid entityId, ImageType type);
 
+        Task RemoveImageAsync(Guid entityId, ImageType type);
     }
 }
