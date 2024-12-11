1be5ee87935605b1c9bc9fb04bbd4f35a1f124ed
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-11 13:23:16 +0200

    Add GetOwnerById feature with query, handler, and exception handling for retrieving owner details by ID

---
+++ TravelAndAccommodationBookingPlatform.Application/Queries/OwnerQueries/GetOwnerByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.OwnerDtos;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Queries.OwnerQueries
+{
+    public class GetOwnerByIdQuery : IRequest<OwnerResponseDto>
+    {
+        public Guid OwnerId { get; init; }
+    }
+}
--- TravelAndAccommodationBookingPlatform.Core/DomainMessages/OwnerMessages.cs
+++ TravelAndAccommodationBookingPlatform.Core/DomainMessages/OwnerMessages.cs
@@ -3,8 +3,6 @@
     public static class OwnerMessages
     {
         public const string OwnerNotFound = "Owner with the provided ID is not found.";
-        public const string EmailAlreadyExists = "An owner with the provided email address already exists.";
-        public const string InvalidOwnerDetails = "The provided owner details are invalid. Please check the input and try again.";
-        public const string DeletionNotAllowed = "The owner cannot be deleted because there are existing hotels or bookings associated with them.";
+
     }
 }
