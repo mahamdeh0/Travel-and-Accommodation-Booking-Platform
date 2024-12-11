2567bb171b7260feb15b79389781de1ac02e931f
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-11 12:03:29 +0200

    Implement CreateOwner command and handler with MediatR, DTOs and mapping profile

---
+++ TravelAndAccommodationBookingPlatform.Application/Handlers/OwnerHandlers/CommandHandlers/CreateOwnerCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.Commands.OwnerCommands;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.OwnerDtos;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.UnitOfWork;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Handlers.OwnerHandlers.CommandHandlers
+{
+    public class CreateOwnerCommandHandler : IRequestHandler<CreateOwnerCommand, OwnerResponseDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateOwnerCommandHandler(IMapper mapper, IOwnerRepository ownerRepository, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _ownerRepository = ownerRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<OwnerResponseDto> Handle(CreateOwnerCommand request, CancellationToken cancellationToken)
+        {
+            var ownerEntity = _mapper.Map<Owner>(request);
+            var createdOwner = await _ownerRepository.AddOwnerAsync(ownerEntity);
+            await _unitOfWork.SaveChangesAsync();
+            return _mapper.Map<OwnerResponseDto>(createdOwner);
+        }
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/OwnerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.OwnerCommands;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.OwnerDtos;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Profiles
+{
+    public class OwnerProfile : Profile
+    {
+        public OwnerProfile()
+        {
+            CreateMap<Owner, OwnerResponseDto>();
+            CreateMap<CreateOwnerCommand, Owner>();
+        }
+    }
+}
