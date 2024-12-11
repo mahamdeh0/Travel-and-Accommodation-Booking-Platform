ecc83f391aec372d8ce5194cced02bd2a611cb54
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-11 16:27:05 +0200

    Add CreateCityCommandHandler and CreateCityCommand implementation with AutoMapper profile for city creation functionality

---
+++ TravelAndAccommodationBookingPlatform.Application/Handlers/CityHandlers/CommandHandlers/CreateCityCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.Commands.CityCommands;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.CityDtos;
+using TravelAndAccommodationBookingPlatform.Core.DomainMessages;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Exceptions;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.UnitOfWork;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Handlers.CityHandlers.CommandHandlers
+{
+    public class CreateCityCommandHandler : IRequestHandler<CreateCityCommand, CityResponseDto>
+    {
+        private readonly ICityRepository _cityRepository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateCityCommandHandler(ICityRepository cityRepository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _cityRepository = cityRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<CityResponseDto> Handle(CreateCityCommand request, CancellationToken cancellationToken)
+        {
+            if (await _cityRepository.ExistsAsync(p => p.PostOffice == request.PostOffice))
+                throw new DuplicateCityPostOfficeException(CityMessages.CityWithPostalCodeExists);
+
+            var newCity = _mapper.Map<City>(request);
+            var createdCity = await _cityRepository.AddAsync(newCity);
+            await _unitOfWork.SaveChangesAsync();
+            return _mapper.Map<CityResponseDto>(createdCity);
+        }
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/CityProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.CityCommands;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.CityDtos;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Profiles
+{
+    public class CityProfile : Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<CreateCityCommand, City>();
+            CreateMap<City, CityResponseDto>();
+        }
+    }
+}
