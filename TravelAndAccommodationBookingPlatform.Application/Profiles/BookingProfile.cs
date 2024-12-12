b6b2444da0ae1d1a9cd2f1b6d63e983dc508cc87
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-12 11:59:15 +0200

    Add Booking DTOs, handlers, mapping profile, and queries

---
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/BookingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.BookingDtos;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Profiles
+{
+    public class BookingProfile : Profile
+    {
+        public BookingProfile()
+        {
+            CreateMap<Booking, BookingResponseDto>()
+                .ForMember(dest => dest.HotelName, opt => opt.MapFrom(src => src.Hotel.Name))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.TotalPrice, opt => opt.MapFrom(src => src.TotalPrice))
+                .ForMember(dest => dest.GuestRemarks, opt => opt.MapFrom(src => src.GuestRemarks))
+                .ForMember(dest => dest.CheckInDate, opt => opt.MapFrom(src => src.CheckInDate))
+                .ForMember(dest => dest.CheckOutDate, opt => opt.MapFrom(src => src.CheckOutDate))
+                .ForMember(dest => dest.BookingDate, opt => opt.MapFrom(src => src.BookingDate));
+        }
+    }
+}
