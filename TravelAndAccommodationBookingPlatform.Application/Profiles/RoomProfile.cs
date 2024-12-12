6d310442bf2e0c838737cb2cebf5cd07d686d9e9
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-12 13:50:10 +0200

    Add CreateRoomCommand, RoomProfile, and CreateRoomHandler for room creation functionality

---
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/RoomProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.RoomCommands;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Profiles
+{
+    public class RoomProfile : Profile
+    {
+        public RoomProfile()
+        {
+            CreateMap<CreateRoomCommand, Room>();
+        }
+    }
+}
