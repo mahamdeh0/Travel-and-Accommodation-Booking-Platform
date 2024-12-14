211b24c416f735dc45c4398d29252a062e3c9dea
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-14 10:27:41 +0200

    Implement DeleteRoomClassCommandHandler with validation for room class existence and associated rooms. Add handling for removal of room classes.

---
+++ TravelAndAccommodationBookingPlatform.Application/Handlers/RoomClassHandlers/CommandHandlers/DeleteRoomClassCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.Commands.RoomClassCommands;
+using TravelAndAccommodationBookingPlatform.Core.DomainMessages;
+using TravelAndAccommodationBookingPlatform.Core.Exceptions;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.UnitOfWork;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Handlers.RoomClassHandlers.CommandHandlers
+{
+    public class DeleteRoomClassCommandHandler : IRequestHandler<DeleteRoomClassCommand>
+    {
+        private readonly IRoomClassRepository _roomClassRepository;
+        private readonly IRoomRepository _roomRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteRoomClassCommandHandler(IRoomClassRepository roomClassRepository, IRoomRepository roomRepository, IUnitOfWork unitOfWork)
+        {
+            _roomClassRepository = roomClassRepository;
+            _roomRepository = roomRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteRoomClassCommand request, CancellationToken cancellationToken)
+        {
+            if (!await _roomClassRepository.ExistsAsync(rc => rc.Id == request.RoomClassId))
+                throw new NotFoundException(RoomClassMessages.RoomClassNotFound);
+
+            if (await _roomRepository.ExistsByPredicateAsync(r => r.RoomClassId == request.RoomClassId))
+                throw new DependentsExistException(RoomClassMessages.ExistingRoomsForRoomClass);
+
+            await _roomClassRepository.RemoveRoomClassAsync(request.RoomClassId);
+
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
