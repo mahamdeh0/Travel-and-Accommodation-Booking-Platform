dcee98852a859c67b5633fec60266de5c2d4c029
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-14 10:04:07 +0200

    Implement CreateRoomClassCommandHandler with validation for hotel existence and unique room class name. Add mapping for CreateRoomClassCommand to RoomClass.

---
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/RoomClassProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.RoomClassCommands;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Profiles
+{
+    public class RoomClassProfile : Profile
+    {
+        public RoomClassProfile()
+        {
+            CreateMap<CreateRoomClassCommand, RoomClass>();
+        }
+    }
+}
