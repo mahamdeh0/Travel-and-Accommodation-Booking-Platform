79e05d1b93d67ce24a1dc3619f0a5cc974429895
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-14 12:22:09 +0200

    Implement GetRoomClassGuestQueryHandler and mapping for RoomClassGuestResponseDto

--- TravelAndAccommodationBookingPlatform.Application/Profiles/RoomClassProfile.cs
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/RoomClassProfile.cs
@@ -12,6 +12,9 @@ namespace TravelAndAccommodationBookingPlatform.Application.Profiles
             CreateMap<CreateRoomClassCommand, RoomClass>();
             CreateMap<UpdateRoomClassCommand, RoomClass>();
             CreateMap<RoomClass, RoomClassManagementResponseDto>().ForMember(dst => dst.ActiveDiscount, options => options.MapFrom(src => src.Discounts.FirstOrDefault()));
+            CreateMap<RoomClass, RoomClassGuestResponseDto>()
+                 .ForMember(dst => dst.ActiveDiscount, options => options.MapFrom(src => src.Discounts.FirstOrDefault()))
+                 .ForMember(dst => dst.Gallery, options => options.MapFrom(src => src.Gallery.Select(i => i.Path)));
         }
     }
 }
