8e7eae82be02018bb8927d410cb5c8c2a39d805b
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-15 11:21:33 +0200

    Add AddGalleryToRoomClassCommandHandler implementation with validation and image upload logic

--- TravelAndAccommodationBookingPlatform.Application/Profiles/RoomClassProfile.cs
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/RoomClassProfile.cs
@@ -2,6 +2,7 @@
 using TravelAndAccommodationBookingPlatform.Application.Commands.RoomClassCommands;
 using TravelAndAccommodationBookingPlatform.Application.DTOs.RoomClassDtos;
 using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
 
 namespace TravelAndAccommodationBookingPlatform.Application.Profiles
 {
@@ -15,6 +16,10 @@ namespace TravelAndAccommodationBookingPlatform.Application.Profiles
             CreateMap<RoomClass, RoomClassGuestResponseDto>()
                  .ForMember(dst => dst.ActiveDiscount, options => options.MapFrom(src => src.Discounts.FirstOrDefault()))
                  .ForMember(dst => dst.Gallery, options => options.MapFrom(src => src.Gallery.Select(i => i.Path)));
+            CreateMap<PaginatedResult<RoomClass>, PaginatedResult<RoomClassGuestResponseDto>>()
+                .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+            CreateMap<PaginatedResult<RoomClass>, PaginatedResult<RoomClassManagementResponseDto>>()
+                .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
         }
     }
 }
