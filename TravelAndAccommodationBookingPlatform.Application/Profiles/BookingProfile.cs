57bcc312ea246293e5951b5e6e44b6dfa5f5d186
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-15 14:22:31 +0200

    Add mapping for RecentlyVisitedHotelResponseDto in BookingProfile

--- TravelAndAccommodationBookingPlatform.Application/Profiles/BookingProfile.cs
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/BookingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using TravelAndAccommodationBookingPlatform.Application.DTOs.BookingDtos;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.HotelDtos;
 using TravelAndAccommodationBookingPlatform.Core.Entities;
 using TravelAndAccommodationBookingPlatform.Core.Models;
 
@@ -9,6 +10,7 @@ namespace TravelAndAccommodationBookingPlatform.Application.Profiles
     {
         public BookingProfile()
         {
+            CreateMap<PaginatedResult<Booking>, PaginatedResult<BookingResponseDto>>();
             CreateMap<Booking, BookingResponseDto>()
                 .ForMember(dest => dest.HotelName, opt => opt.MapFrom(src => src.Hotel.Name))
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
@@ -17,8 +19,18 @@ namespace TravelAndAccommodationBookingPlatform.Application.Profiles
                 .ForMember(dest => dest.CheckInDate, opt => opt.MapFrom(src => src.CheckInDate))
                 .ForMember(dest => dest.CheckOutDate, opt => opt.MapFrom(src => src.CheckOutDate))
                 .ForMember(dest => dest.BookingDate, opt => opt.MapFrom(src => src.BookingDate));
-
-            CreateMap<PaginatedResult<Booking>, PaginatedResult<BookingResponseDto>>();
+            CreateMap<Booking, RecentlyVisitedHotelResponseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Hotel.Id))
+                .ForMember(dest => dest.BookingId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Hotel.Name))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Hotel.City.Name))
+                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Hotel.City.Country))
+                .ForMember(dest => dest.StarRating, opt => opt.MapFrom(src => src.Hotel.StarRating))
+                .ForMember(dest => dest.ReviewsRating, opt => opt.MapFrom(src => src.Hotel.ReviewsRating))
+                .ForMember(dest => dest.TotalPrice, opt => opt.MapFrom(src => src.TotalPrice))
+                .ForMember(dest => dest.Thumbnail, opt => opt.MapFrom(src => src.Hotel.Thumbnail != null ? src.Hotel.Thumbnail.Path : null))
+                .ForMember(dest => dest.CheckInDate, opt => opt.MapFrom(src => src.CheckInDate))
+                .ForMember(dest => dest.CheckOutDate, opt => opt.MapFrom(src => src.CheckOutDate));
         }
     }
 }
