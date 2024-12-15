04f8f956320b39e12e175907a531192fdbaae4d6
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-15 13:50:12 +0200

    Add AddHotelThumbnailCommand and its handler for managing hotel thumbnails

---
+++ TravelAndAccommodationBookingPlatform.Application/Commands/HotelCommands/AddHotelThumbnailCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Commands.HotelCommands
+{
+    public class AddHotelThumbnailCommand : IRequest
+    {
+        public Guid HotelId { get; init; }
+        public IFormFile Image { get; init; }
+    }
+}
