926916afb141e30b21db316132e7d4f9ac82ea38
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-16 11:03:59 +0200

    Add Pagination and Sorting extensions for IQueryable. Includes GetPage, GetPaginationMetadataAsync, and Sort methods.

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Extensions/SortingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Extensions
+{
+    public static class SortingExtensions
+    {
+        public static IQueryable<T> Sort<T>(this IQueryable<T> queryable, string? sortColumn, OrderDirection sortDirection)
+        {
+            if (string.IsNullOrEmpty(sortColumn)) return queryable;
+
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var property = Expression.PropertyOrField(parameter, sortColumn);
+            var lambda = Expression.Lambda<Func<T, object>>(Expression.Convert(property, typeof(object)), parameter);
+
+            return sortDirection == OrderDirection.Ascending ? queryable.OrderBy(lambda) : queryable.OrderByDescending(lambda);
+        }
+    }
+}
