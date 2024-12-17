e1c49ba3c0c08ad3eceb37bbe76d68e614ebae3c
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-17 10:45:31 +0200

    Add LoginRequestDto and RegisterRequestDto to Authentication DTOs

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/DTOs/Authentication/RegisterRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Authentication
+{
+    public class RegisterRequestDto
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
