c571db472b5416ac3a012cfd60ae069b55701827
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-18 12:21:51 +0200

    Add DTOs for Review operations: GetReviewsRequestDto, ReviewCreationRequestDto, and ReviewUpdateRequestDto

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/DTOs/Reviews/ReviewUpdateRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Reviews
+{
+    public class ReviewUpdateRequestDto
+    {
+        public string Content { get; init; }
+        public int Rating { get; init; }
+    }
+}
