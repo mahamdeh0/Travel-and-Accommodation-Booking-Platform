8d296f8b7f0f9b1f68ff06a3232059d3d275f6d5
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-18 11:49:14 +0200

    Add DTOs for Room Class operations: GetRoomClassesGuestRequestDto, GetRoomClassesRequestDto, RoomClassCreationRequestDto, and RoomClassUpdateRequestDto

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/DTOs/RoomClasses/RoomClassCreationRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAndAccommodationBookingPlatform.WebAPI.DTOs.RoomClasses
+{
+    public class RoomClassCreationRequestDto
+    {
+        public Guid HotelId { get; init; }
+        public string Name { get; init; }
+        public int MaxChildrenCapacity { get; init; }
+        public int MaxAdultsCapacity { get; init; }
+        public decimal NightlyRate { get; init; }
+        public string? Description { get; init; }
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.WebAPI/DTOs/RoomClasses/RoomClassUpdateRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelAndAccommodationBookingPlatform.WebAPI.DTOs.RoomClasses
+{
+    public class RoomClassUpdateRequestDto
+    {
+        public string Name { get; init; }
+        public int MaxChildrenCapacity { get; init; }
+        public int MaxAdultsCapacity { get; init; }
+        public decimal NightlyRate { get; init; }
+        public string? Description { get; init; }
+    }
+}
