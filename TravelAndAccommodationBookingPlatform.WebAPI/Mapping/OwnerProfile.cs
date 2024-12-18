bf2b58559a87c4675d9c96f3cc8c9baf40ad7191
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-18 11:00:28 +0200

    Add OwnerProfile class for AutoMapper configurations, mapping DTOs to commands and queries

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/Mapping/OwnerProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.OwnerCommands;
+using TravelAndAccommodationBookingPlatform.Application.Queries.OwnerQueries;
+using TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Owners;
+using TravelAndAccommodationBookingPlatform.WebAPI.Helpers;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Mapping
+{
+    public class OwnerProfile : Profile
+    {
+        public OwnerProfile()
+        {
+            CreateMap<OwnerCreationRequestDto, CreateOwnerCommand>();
+            CreateMap<OwnerUpdateRequestDto, UpdateOwnerCommand>();
+            CreateMap<GetOwnersRequestDto, GetOwnersQuery>()
+                .ForMember(
+                    dst => dst.OrderDirection,
+                    opt => opt.MapFrom(src => MappingHelpers.MapOrderDirection(src.OrderDirection)));
+        }
+    }
+}
