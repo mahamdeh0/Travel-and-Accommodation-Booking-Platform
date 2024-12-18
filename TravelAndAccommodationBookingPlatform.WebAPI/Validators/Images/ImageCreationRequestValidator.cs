99b5d691ae2823888386a8a52ff89622b661112f
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-18 15:18:18 +0200

    Add FluentValidation validator for Image creation requests, validating image file and type

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/Validators/Images/ImageCreationRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Images;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Validators.Images
+{
+    public class ImageCreationRequestValidator : AbstractValidator<ImageCreationRequestDto>
+    {
+        private readonly string[] allowedImageTypes = { "image/jpg", "image/jpeg", "image/png" };
+
+        public ImageCreationRequestValidator()
+        {
+            RuleFor(x => x.Image)
+                .NotNull().WithMessage("Image file is required.")
+                .Must(image => image.Length > 0).WithMessage("Image file must not be empty.")
+                .Must(image => IsAllowedImageType(image.ContentType))
+                .WithMessage("Image file must be of type JPG, JPEG, or PNG.");
+        }
+
+        private bool IsAllowedImageType(string contentType)
+        {
+            return allowedImageTypes.Contains(contentType);
+        }
+    }
+}
