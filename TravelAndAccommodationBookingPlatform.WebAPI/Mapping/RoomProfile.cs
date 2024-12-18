8dc0b3d1ffca42a525ef6e628d684279585f4ea8
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-18 12:06:13 +0200

    Add RoomProfile for AutoMapper configurations to map Room DTOs to commands and queries

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/Mapping/RoomProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.RoomCommands;
+using TravelAndAccommodationBookingPlatform.Application.Queries.RoomQueries;
+using TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Rooms;
+using TravelAndAccommodationBookingPlatform.WebAPI.Helpers;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Mapping
+{
+    public class RoomProfile : Profile
+    {
+        public RoomProfile()
+        {
+            CreateMap<RoomCreationRequestDto, CreateRoomCommand>();
+            CreateMap<RoomUpdateRequestDto, UpdateRoomCommand>();
+            CreateMap<GetRoomsRequestDto, GetRoomsManagementQuery>()
+              .ForMember(
+                    dst => dst.OrderDirection,
+                    opt => opt.MapFrom(src => MappingHelpers.MapOrderDirection(src.OrderDirection))); CreateMap<GetRoomsGuestsRequestDto, GuestRoomsByClassIdQuery>();
+        }
+    }
+}
