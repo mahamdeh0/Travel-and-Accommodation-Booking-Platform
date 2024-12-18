fd23090bf896345f802dbbcb5d0b75acb265bac3
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-18 15:17:15 +0200

    Add FluentValidation validators for City creation, update, and retrieval requests

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/Validators/Cities/CityCreationRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Cities;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Validators.Cities
+{
+    public class CityCreationRequestValidator : AbstractValidator<CityCreationRequestDto>
+    {
+        public CityCreationRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("City name is required.");
+
+            RuleFor(x => x.Country)
+                .NotEmpty().WithMessage("Country is required.");
+
+            RuleFor(x => x.Region)
+                .NotEmpty().WithMessage("Region is required.");
+
+            RuleFor(x => x.PostOffice)
+                .NotEmpty().WithMessage("Post office is required.");
+        }
+    }
+}
