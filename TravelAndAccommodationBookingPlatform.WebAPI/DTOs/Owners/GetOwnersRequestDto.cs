cf3edc849bd000a5b1225b068a61ee99b13d0863
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-18 10:58:44 +0200

    Add DTOs for Owner operations: GetOwnersRequestDto, OwnerCreationRequestDto, and OwnerUpdateRequestDto

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/DTOs/Owners/GetOwnersRequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Owners
+{
+    public class GetOwnersRequestDto
+    {
+        public string? OrderDirection { get; init; }
+        public string? SortColumn { get; init; }
+        public string? Search { get; init; }
+        public int PageNumber { get; init; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            init => _pageSize = Math.Min(value, 20);
+        }
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.WebAPI/DTOs/Owners/OwnerCreationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Owners
+{
+    public class OwnerCreationRequestDto
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Email { get; init; }
+        public string PhoneNumber { get; init; }
+    }
+}
