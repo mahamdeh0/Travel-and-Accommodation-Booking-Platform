6c48573e09fd8bf2c0dc1e6a9d0bcf449ee5fc15
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-19 12:00:26 +0200

    Implement rate limiting using a fixed window strategy with configurable options

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/RateLimiting/RateLimiterConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.RateLimiting
+{
+    public static class RateLimiterConfiguration
+    {
+        public static IServiceCollection AddCustomRateLimiter(this IServiceCollection services)
+        {
+            services.Configure<YourNamespace.RateLimiting.RateLimiterOptions>(options =>
+            {
+                options.MaxRequests = 100;
+                options.WindowDurationInSeconds = 60;
+            });
+
+            services.AddRateLimiter(options =>
+            {
+                var provider = services.BuildServiceProvider();
+                var rateLimiterSettings = provider.GetRequiredService<IOptions<YourNamespace.RateLimiting.RateLimiterOptions>>().Value;
+
+                options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+                options.AddFixedWindowLimiter("FixedWindowPolicy", config =>
+                {
+                    config.PermitLimit = rateLimiterSettings.MaxRequests;
+                    config.Window = TimeSpan.FromSeconds(rateLimiterSettings.WindowDurationInSeconds);
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/RateLimiting/RateLimiterOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Infrastructure.RateLimiting
+{
+    namespace YourNamespace.RateLimiting
+    {
+        public class RateLimiterOptions
+        {
+            public int MaxRequests { get; set; } = 100;
+            public int WindowDurationInSeconds { get; set; } = 60;
+        }
+    }
+}
