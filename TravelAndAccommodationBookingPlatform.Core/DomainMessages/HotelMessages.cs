19af8959842f16680a4630651b6689b815b1eea5
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-19 12:39:54 +0200

    Update domain messages classes: added and removed specific messages for better clarity and functionality

--- TravelAndAccommodationBookingPlatform.Core/DomainMessages/HotelMessages.cs
+++ TravelAndAccommodationBookingPlatform.Core/DomainMessages/HotelMessages.cs
@@ -6,6 +6,5 @@
         public const string HotelHasDependents = "The specified hotel still has dependent entities associated with it. Please remove them first.";
         public const string InvalidStarRating = "The star rating must be between 1 and 5.";
         public const string HotelWithSameLocationExists = "A hotel already exists at the same location (longitude, latitude).";
-        public const string HotelClosed = "The hotel is currently closed and cannot accept new bookings.";
     }
 }
