54ae39ffcab61dfc1cc32c225ea280ff95f2438d
mahamdeh0 <mahamdehabd@gmail.com>
2024-12-21 17:01:41 +0200

    Add extension methods for API Versioning, FluentValidation, Swagger, and Web API configuration.

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerSetup(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(setup =>
+            {
+                setup.SwaggerDoc("v1", new OpenApiInfo { Title = "TravelAndAccommodationBookingPlatform", Version = "v1" });
+
+                setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT token here to access the API"
+                });
+
+                setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                       Reference = new OpenApiReference
+                       {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                       }
+                     },
+                     Array.Empty<string>()
+                   }
+                });
+
+                setup.UseDateOnlyTimeOnlyStringConverters();
+            });
+
+            return services;
+        }
+    }
+}
